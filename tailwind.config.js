a3c525ea8328adeb6b8d7cea0ba282d49d65ecf1
imranrkhan13 <muhammadimrank034@gmail.com>
2026-02-03 00:42:56 +0530

    important

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
-module.exports = {
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        peach: {
-          bg: '#FFF5F0',
-          bgLight: '#FFF7F2',
-          text: '#4A3835',
-          accent: '#C97A63',
-          gradStart: '#FFEBE3',
-          gradEnd: '#FFE0D4',
-        }
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
-}
+  plugins: [],
+};
